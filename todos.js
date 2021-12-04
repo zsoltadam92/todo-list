d144babfe03bbd513a71e7f81658213aefcbf830
zsoltadam92 <zsoltadam92@gmail.com>
2021-12-04 01:11:21 +0100

    increase,decrease,difficult

--- todos.js
+++ todos.js
@@ -1,27 +1,7 @@
 import { calculateCompleted, uuidv4 } from "./calculator.js";
 
-// const todo1 = {
-//   id: uuidv4(),
-//   name: 'alma',
-//   isComplete: false,
-// }
-// const todo2 = {
-//   id: uuidv4(),
-//   name: 'körte',
-//   isComplete: false,
-// }
-// const todo3 = {
-//   id: uuidv4(),
-//   name: 'banán',
-//   isComplete: false,
-// }
 
-const todos = [
-  // todo1,
-  // todo2,
-  // todo3,
-]
-
+const todos = []
 
 export const initTodos = () => {
   displayTodos();
@@ -29,20 +9,32 @@ export const initTodos = () => {
   addListItem();
 
   updateProgress()
+
+  sortList()
 };
 
 const displayTodos = () => {
   const containerTodos = document.querySelector('.todos-container')
   containerTodos.innerHTML = "";
 
+  // legnehezebb elem
+  const hardestTodo = document.querySelector('#hardestItem')
+  hardestTodo.innerHTML = ''
+  let difficultItems = todos.map(todo => todo.difficulty)
+
   todos.forEach(todo => {
     containerTodos.innerHTML += `
     <div class="todo-box">
       <input type="checkbox" ${todo.isComplete ? 'checked' : ''}>
-      <span class="${todo.isComplete ? 'complete' : ' incomplete'} list-item">${todo.name}</span>
+      <span class="${todo.isComplete ? 'complete' : ' incomplete'} list-item">${todo.name} - ${todo.difficulty}</span>
       <i class="fas fa-trash-alt delete-button" data-todoid="${todo.id}"></i>
     </div>
     `
+
+    // legnehezebb elem megjelenitése
+    if(todo.difficulty === Math.max(...difficultItems)) {
+      hardestTodo.innerHTML =`${todo.name} - ${Math.max(...difficultItems)}`
+    }
   })
 
   checkboxEvents()
@@ -50,23 +42,57 @@ const displayTodos = () => {
   deleteListItem()
 }
 
+// nehézségi szint validálása
+const validateDifficult = () => {
+  const todoDifficulty = document.querySelector('#difficulty')
+  const error = document.querySelector('#difficultNumberError');
+  const regex = /[^1-5]+/;
+
+  todoDifficulty.addEventListener('input', (e) => {
+    const value = e.target.value;
+
+    if (regex.test(value)) {
+      todoDifficulty.value = value.slice(0, value.length - 1);
+      error.textContent = 'Csak 1 és 5 közötti szám adható meg!';
+    } else {
+      error.textContent = '';
+    }
+  });
+}
 
 //új elem hozzáadása
 const addListItem = () => {
-  document.querySelector('.add-button').addEventListener('click', () => {
-    const todoWrite = document.querySelector('.todo-write')
-    let name = todoWrite.value
-    let isComplete = false
   
+  validateDifficult()
+
+  document.querySelector('.add-button').addEventListener('click', () => {
+    const todoThing = document.querySelector('#todo-thing')
+    const todoDifficulty = document.querySelector('#difficulty')
+    
+    let name = todoThing.value
+    let difficulty = Number(todoDifficulty.value)
+    let isComplete = false
+
+    //üresen hagyott mező figyelmeztetés
+    if (name == "" || difficulty == "") {
+      document.querySelector('#emptyInput').textContent = "Töltse ki az üres mezőket!"
+      return false;
+    } else {
+      document.querySelector('#emptyInput').textContent = ""
+    }
+    
+    // hozzáadja az új elemet
     todos.push({
       id: uuidv4(),
       name: name,
-      isComplete: isComplete
+      difficulty: difficulty,
+      isComplete: isComplete,
     })
     
     updateProgress()
     displayTodos()
-    todoWrite.value = ""
+    todoThing.value = ""
+    todoDifficulty.value = ""
   })
 }
 
@@ -114,5 +140,52 @@ const deleteListItem = () =>{
 }
 
 
+// növekvő sorrend
+const increaseList = () => {
+  todos.sort((a,b) => {
+    let nameA = a.name.toUpperCase()
+    let nameB = b.name.toUpperCase()
+  
+    if(nameA < nameB) {
+      return -1
+    }
+    if(nameA > nameB) {
+      return 1
+    }
+  
+    return 0
+  })
+}
+
+// csökkenő sorrend
+const decreaseList = () => {
+  todos.sort((a,b) => {
+    let nameA = a.name.toUpperCase()
+    let nameB = b.name.toUpperCase()
+  
+    if(nameA > nameB) {
+      return -1
+    }
+    if(nameA < nameB) {
+      return 1
+    }
+  
+    return 0
+  })
+}
+
+const sortList = () => {
+  const increaseButton = document.getElementById('increase')
+  const decreaseButton = document.getElementById('decrease')
+
+  increaseButton.addEventListener('click', () => {
+    increaseList()
+    displayTodos()
+  })
+  decreaseButton.addEventListener('click',  ()=> {
+    decreaseList()
+    displayTodos()
+  })
+}
 
 
